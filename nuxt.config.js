d12c03b7029625f2da2423864c4db754991ba89c
Guy Levi <robozevel@gmail.com>
2019-06-02 19:24:44 +0300

    font loading and robots.txt

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,9 @@ export default {
   },
   head: {
     title,
+    htmlAttrs: {
+      lang: 'en-US'
+    },
     meta: [
       { hid: 'viewport', name: 'viewport', content: 'width=device-width,initial-scale=1' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
@@ -18,7 +21,9 @@ export default {
       { hid: 'og:description', property: 'og:description', content: description }
     ],
     link: [
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Lato' }
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Lato&display=swap' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
+      { rel: 'preconnect', href: 'https://www.google-analytics.com' }
     ]
   },
   loading: {
@@ -26,6 +31,7 @@ export default {
     throttle: 100
   },
   modules: [
+    '@nuxtjs/robots',
     ['@nuxtjs/google-analytics', {
       id: 'UA-139919699-2'
     }]
